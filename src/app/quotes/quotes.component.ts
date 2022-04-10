bbfc97eb91683db485e76930df09f4e8b0fae005
Wanjiru393 <carolciru393@gmail.com>
2022-04-10 12:23:41 +0300

    Implemented highlight logic

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,12 +11,15 @@ export class QuotesComponent implements OnInit {
 
   quotes: Quote[] =[
     new Quote(1,'"And, when you want something, all the universe conspires in helping you to achieve it.” — Paulo Coelho',0,0),
-    new Quote(2,'“Be the change that you wish to see in the world.”-Mahatma Gandhi',0,1),
+    new Quote(2,'“Be the change that you wish to see in the world.”-Mahatma Gandhi',4,1),
     new Quote(3,'“Do not take life too seriously,live. You will never get out of it alive.” —Elbert Geoffry Hubbard',0,0),
     new Quote(4,'"Failure is another stepping-stone to greatness."-Oprah Winfrey',0,0),
     new Quote(5,'"Sometimes you can not see yourself clearly until you see yourself through the eyes of others."-Ellen DeGeneres',0,0)
 
   ]
+
+  arr: number[] =this.quotes.map(quote=>quote.upvotes)
+  highest = Math.max(...this.arr)
 
   constructor() { }
 
