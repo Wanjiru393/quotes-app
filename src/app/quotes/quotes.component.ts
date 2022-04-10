45edce4078a56339517a0ed762b5fadd76ddf454
Wanjiru393 <carolciru393@gmail.com>
2022-04-10 03:29:17 +0300

    Added Upvote & Downvote Button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,9 +10,9 @@ export class QuotesComponent implements OnInit {
 
 
   quotes: Quote[] =[
-    new Quote(1,'"And, when you want something, all the universe conspires in helping you to achieve it.” — Paulo Coelho, The Alchemist',0,0),
-    new Quote(2,'“Be the change that you wish to see in the world.”-Mahatma Gandhi',1,1),
-    new Quote(3,'“Do not take life too seriously. You will never get out of it alive.” —Elbert Hubbard',0,0),
+    new Quote(1,'"And, when you want something, all the universe conspires in helping you to achieve it.” — Paulo Coelho',0,0),
+    new Quote(2,'“Be the change that you wish to see in the world.”-Mahatma Gandhi',0,1),
+    new Quote(3,'“Do not take life too seriously,live. You will never get out of it alive.” —Elbert Geoffry Hubbard',0,0),
     new Quote(4,'"Failure is another stepping-stone to greatness."-Oprah Winfrey',0,0),
     new Quote(5,'"Sometimes you can not see yourself clearly until you see yourself through the eyes of others."-Ellen DeGeneres',0,0)
 
